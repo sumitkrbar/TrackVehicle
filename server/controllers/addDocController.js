e8c4e3c20c7ca69ba321195755daf972a893cd48
Sumit Kumar Baranwal <sumitkb2004@gmail.com>
2025-10-26 12:34:22 +0530

    Added add-doc controller

--- server/controllers/addDocController.js
+++ server/controllers/addDocController.js
@@ -1,7 +1,30 @@
+import Document from '../models/document.js';
 export const addDocController = async (req, res) =>{
+    console.log("hereeee");
+    
     try{
+        const {owner, phone, vehicleNumber, cf, np, auth, remarks} = req.body;
+        console.log("request recieved")
+        console.log(req.body);
         
+        const newDocument = new Document({
+            owner,
+            phone,
+            vehicleNumber,
+            cf,
+            np,
+            auth,
+            remarks
+        });
+        console.log("doc created");
+        
+        await newDocument.save();
+        console.log("saved");
+        
+        res.status(201).json({ message: 'Document added successfully', document: newDocument });
     }catch(error){
+        console.log(error);
+        
         res.status(500).json({ message: 'Server Error' });
     }   
 }
